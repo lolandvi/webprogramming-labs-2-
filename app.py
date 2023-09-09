627add982986f3be6980bf0368ad2f6b26aecd6d
Lolita <shinkava13@gmail.com>
2023-09-09 15:55:48 +0700

    Статические файлы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -54,3 +54,18 @@ Werkzeug, а также шаблонизатор Jinja2. Относится к �
     </body>
 </html>    
 """
+
+@app.route("/lab1/oak")
+def oak():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''"
+    </head>
+    <body>
+        <h1>Дуб</h1>
+        <img src="''' + url_for('static', filename='oak.png') + '''">
+    </body>
+</html>    
+'''
