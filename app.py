3bbad52222c37f089ca4b638be44f7bc392fc5ef
Lolita <shinkava13@gmail.com>
2023-09-09 15:19:28 +0700

    Добавление в .gitignore

--- app.py
+++ app.py
@@ -4,4 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-server on the flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Ветрова, Клопенкова, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-server on the flask</h1>
+
+        <footer>
+            &copy; Ветрова, Клопенкова, ФБИ-11, 3 курс, 2023
+        </footer>    
+    </body>
+</html>    
+"""
